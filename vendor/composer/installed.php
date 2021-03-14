f3d4cfa4be61dc6c0920d4aaf9cb036d45fbf616
Jake <jakewarrenblack01@gmail.com>
2021-03-14 16:43:40 +0000

    Reinstall nikic/fast-route and stripe. Payment form on checkout page now creates stripe customer object with charge. Submits customer payment data to stripe dashboard.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => 'b49149c6210c7ae1ddfe688c415c10b9155f0eb8',
+    'reference' => '507df8b750a35ef4bc30dcccd77f40470f5d3e09',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => 'b49149c6210c7ae1ddfe688c415c10b9155f0eb8',
+      'reference' => '507df8b750a35ef4bc30dcccd77f40470f5d3e09',
     ),
     'graham-campbell/result-type' => 
     array (
@@ -55,49 +55,6 @@
       array (
       ),
       'reference' => '994ecccd8f3283ecf5ac33254543eb0ac946d525',
-    ),
-    'pimple/pimple' => 
-    array (
-      'pretty_version' => 'v3.4.0',
-      'version' => '3.4.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '86406047271859ffc13424a048541f4531f53601',
-    ),
-    'psr/container' => 
-    array (
-      'pretty_version' => '1.1.1',
-      'version' => '1.1.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '8622567409010282b7aeebe4bb841fe98b58dcaf',
-    ),
-    'psr/http-message' => 
-    array (
-      'pretty_version' => '1.0.1',
-      'version' => '1.0.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
-    ),
-    'psr/http-message-implementation' => 
-    array (
-      'provided' => 
-      array (
-        0 => '1.0',
-      ),
-    ),
-    'slim/slim' => 
-    array (
-      'pretty_version' => '3.12.3',
-      'version' => '3.12.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '1c9318a84ffb890900901136d620b4f03a59da38',
     ),
     'stripe/stripe-php' => 
     array (
