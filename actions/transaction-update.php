0c3fea081c8e1769cf2b13930084b9fca0e593db
Jake <jakewarrenblack01@gmail.com>
2021-04-07 23:45:03 +0100

    Add transaction update form and script. Fix Transaction::save to allow for updating Transactions.

---
+++ actions/transaction-update.php
@@ -0,0 +1,49 @@
+<?php
+require_once '../config.php';
+use BookWorms\Model\Transaction;
+use BookWorms\Model\Customer;
+
+try {
+    $status = [
+        "succeeded", "failed"
+    ];
+
+    $rules = [
+        "transaction_id" => "present",
+        "customer_id" => "present|integer|minlength:1",
+        "status" => "present|in:" . implode(',', $status),
+        "date" => "present",
+        "total" => "present|integer|minlength:1"
+    ];
+
+    $request->validate($rules);
+    if ($request->is_valid()) {
+        $transaction = Transaction::findById($request->input("transaction_id"));
+        $transaction->customer_id = $request->input("customer_id");
+        $transaction->status = $request->input("status");
+        $transaction->date = $request->input("date");
+        $transaction->total = $request->input("total");
+
+        $transaction->save();
+        $request->session()->set("flash_message", "The transaction was successfully updated in the database");
+        $request->session()->set("flash_message_class", "alert-info");
+        /*Forget any data that's already been stored in the session.*/
+        $request->session()->forget("flash_data");
+        $request->session()->forget("flash_errors");
+        $request->redirect("/index.php");
+    } 
+    else {
+        $transaction = $request->input("transaction_id");
+        $request->session()->set("flash_data", $request->all());
+        $request->session()->set("flash_errors", $request->errors());
+        $request->redirect("../views/admin/transactions/transaction-edit.php?transaction_id=" . $transaction_id);
+        
+    }
+} catch (Exception $ex) {
+    $request->session()->set("flash_message", $ex->getMessage());
+    $request->session()->set("flash_message_class", "alert-warning");
+    $request->session()->set("flash_data", $request->all());
+    $request->session()->set("flash_errors", $request->errors());
+
+    $request->redirect("/index.php");
+}
