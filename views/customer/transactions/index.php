4625e2396e460b591e3c6dc4b968070c41e25f62
Jake Black <jakewarrenblack01@gmail.com>
2021-04-10 02:02:05 +0100

    Transaction_timber::findbycustomerid now returns array. Now printing timber species via transaction_timber records on customer profile page

--- views/customer/transactions/index.php
+++ views/customer/transactions/index.php
@@ -1,38 +1,57 @@
 <?php
-    use BookWorms\Model\Transaction;
-    $customer_id = $request->session()->get("customer_id");
 
-    try{
-        $transactions = Transaction::findByCustomerId($customer_id);
-        $numProducts = count($transactions);
-        $pageSize = 10;
-        $numPages = ceil($numProducts / $pageSize);
-    }
-    catch(Exception $e){
-        $request->session()->set("flash_message", "No previous transactions.");
-    }
+use BookWorms\Model\Transaction;
+use BookWorms\Model\Transaction_Timber;
+use BookWorms\Model\Timber;
+
+$customer_id = $request->session()->get("customer_id");
+
+try {
+    $transactions = Transaction::findByCustomerId($customer_id);
+    $numProducts = count($transactions);
+    $pageSize = 10;
+    $numPages = ceil($numProducts / $pageSize);
+} catch (Exception $e) {
+    $request->session()->set("flash_message", "No previous transactions.");
+}
 ?>
-    <table class="table" id="table-transactions">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Species</th>
-                <th>description</th>
-                <th>Price</th>
+<table class="table" id="table-transactions">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Species</th>
+            <th>description</th>
+            <th>Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($transactions as $transaction) { ?>
+            <tr class="d-none">
+                <td><?= $transaction->id ?></td>
+                <td>
+                    <?php
+                    $transaction_timbers = Transaction_Timber::findByTransactionId($transaction->id);
+                    $timbers = array();
+
+                    foreach ($transaction_timbers as $transaction_timber) {
+                        $timber_obj = Timber::findById($transaction_timber->timber_id);
+                        $timbers[] = $timber_obj->title;
+                    }
+
+                    if (count($timbers) > 1) {
+                        echo implode(", ", $timbers);
+                    } else {
+                        echo $timbers[0];
+                    }
+                    ?>
+                </td>
+                <td><?= $transaction->status ?></td>
+                <td><?= $transaction->date ?></td>
+                <td><?= $transaction->total ?></td>
             </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($transactions as $transaction) { ?>
-                <tr class="d-none">
-                    <td><?= $transaction->id ?></td>
-                    <td><?= $transaction->customer_id ?></td>
-                    <td><?= $transaction->status ?></td>
-                    <td><?= $transaction->date ?></td>
-                    <td><?= $transaction->total ?></td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+        <?php } ?>
+    </tbody>
+</table>
 
 <nav id="nav-transactions">
     <ul class="pagination justify-content-center">
