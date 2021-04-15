19bf98e4511d5a9883c1c5def2f7b9b36216eda8
Jake Black <jakewarrenblack01@gmail.com>
2021-04-15 19:02:34 +0100

    Clear cart after transaction completes.

--- actions/charge.php
+++ actions/charge.php
@@ -85,7 +85,6 @@ if ($request->is_logged_in()) {
             // if no customer exists for this user->id, this must be an admin. only customers can make purchases.
             $request->session()->set("flash_message", "Only customers may place orders. You are logged in as an administrator.");
             $request->session()->set("flash_message_class", "alert-warning");
-            $cart->empty();
             $request->redirect("/index.php");
         }
     }
@@ -96,7 +95,9 @@ if ($request->is_logged_in()) {
     $request->redirect("/index.php");
 }
 
-
+// Always empty the cart after a transaction is completed
+$cart->empty();
+unset($_SESSION['cart']);
 //Redirect to success page, passing charge_id and description to be retrieved via $_GET
 $request->redirect("/views/success.php?tid=" . $charge->id . "&product=" . $charge->description);
 
