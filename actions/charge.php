a773063a48a100d6898e03270ae600a16b7453b2
Jake Black <jakewarrenblack01@gmail.com>
2021-04-27 23:19:44 +0100

    All phpmailer files were somehow removed, added them back. Update charge.php rule

--- actions/charge.php
+++ actions/charge.php
@@ -12,8 +12,7 @@ use BookWorms\Model\Timber;
 
 $rules = [
     "email" => "present|email|minlength:7|maxlength:64",
-    "customer_id" => "present|integer|minlength:1",
-    "fullname" => "present|minlength:4|maxlength:64",
+    "name" => "present|minlength:4|maxlength:64",
     "address" => "present|minlength:8|maxlength:64",
     "phone" => "present|match:/\A[0-9]{2,3}[-][0-9]{5,7}\Z/",
     "subtotal" => "present|integer"
@@ -22,112 +21,111 @@ $rules = [
 $request->validate($rules);
 if ($request->is_valid()) {
 
-$cart = Cart::get($request);
+    $cart = Cart::get($request);
 
-// create StripeClient instance using my API key
-$stripe = new \Stripe\StripeClient('sk_test_51IUfgkLBrNI420twkT9IILtHne9NDCKgXikENZHVRaBfp7nfV7zgNyJKUUh0rB7B8RLAdJqVmpOriEOe5gK6Ggoc000g422Jhc');
+    // create StripeClient instance using my API key
+    $stripe = new \Stripe\StripeClient('sk_test_51IUfgkLBrNI420twkT9IILtHne9NDCKgXikENZHVRaBfp7nfV7zgNyJKUUh0rB7B8RLAdJqVmpOriEOe5gK6Ggoc000g422Jhc');
 
-// retrieve data from checkout form
-$email = $request->input("email");
-$fullname = $request->input("fullname");
-$phone = $request->input("phone");
-$address1 = $request->input("address");
-// Stripe formatting needs this .00 appended
-$subtotal = intval($request->session()->get("subtotal") . "00");
-$token = $request->input("stripeToken");
+    // retrieve data from checkout form
+    $email = $request->input("email");
+    $fullname = $request->input("fullname");
+    $phone = $request->input("phone");
+    $address1 = $request->input("address");
+    // Stripe formatting needs this .00 appended
+    $subtotal = intval($request->session()->get("subtotal") . "00");
+    $token = $request->input("stripeToken");
 
-// we're going to get all the product titles to use as our Stripe charge object's description
-// eg a transaction description could be 'Iroko, American Alder, Ash'
-$description = [];
-foreach ($cart->items as $item) {
-    // push timber->title under key to $description array
-    array_push($description, $item->timber->title);
-}
-// implode will join the array by the ", " separator
-$description_result = implode(", ", $description);
-
-//Create a Stripe customer
-$customer = $stripe->customers->create([
-    'email' => $email,
-    'source' => $token
-]);
-
-//Charge the customer
-$charge = $stripe->charges->create([
-    'amount' => $subtotal,
-    'currency' => 'eur',
-    'description' => $description_result,
-    'customer' => $customer->id
-]);
-
-$total_quantity = 0;
-
-foreach ($cart->items as $item) {
-    $total_quantity += intval($item->quantity);
-}
-
-
-if ($request->is_logged_in()) {
-    if (User::findByEmail($email) != null) {
-        $user = User::findByEmail($email);
-        //just add this transaction_id to the existing customer
-        if (Customer::findByUserID($user->id) != null) {
-            $customer = Customer::findByUserID($user->id);
-
-            //create new transaction and apply to existing customer
-            // the transaction table will provide info on the overall transaction and link to the customer who made the purchase
-            $transaction = new Transaction();
-            $transaction->id = $charge->id;
-            $transaction->customer_id = $customer->id;
-            $transaction->status = $charge->status;
-            $transaction->date = date("Y-m-d H:i:s");
-            $transaction->total = intval($request->session()->get("subtotal"));
-            $transaction->save();
-
-            // create transaction_timber record
-            // looping through products involved in this transaction
-            foreach ($cart->items as $item) {
-                // transaction_timber table will store a record for each product involved in our transaction
-                $transaction_timber = new Transaction_Timber();
-                $transaction_timber->quantity = $item->quantity;
-                $transaction_timber->profiling = $item->profiling;
-                $transaction_timber->sqfootage = $item->sqfootage;
-
-                // Database needs a 0 or 1, our checkbox passes 'on' or null, so change it to suit:
-                $fire_rated = null;
-                if ($item->fire_rated === null) {
-                    $fire_rated = 0;
-                } else {
-                    $fire_rated = 1;
-                }
-
-                $transaction_timber->fire_rated = $fire_rated;
-                $transaction_timber->transaction_id = $charge->id;
-                $transaction_timber->timber_id = (Timber::findByTitle($item->timber->title))->id;
-                $transaction_timber->save();
-            }
-        } else {
-            // if no customer exists for this user->id, this must be an admin. only customers can make purchases.
-            $request->session()->set("flash_message", "Only customers may place orders. You are logged in as an administrator.");
-            $request->session()->set("flash_message_class", "alert-warning");
-            $request->redirect("/views/checkout.php");
-        }
+    // we're going to get all the product titles to use as our Stripe charge object's description
+    // eg a transaction description could be 'Iroko, American Alder, Ash'
+    $description = [];
+    foreach ($cart->items as $item) {
+        // push timber->title under key to $description array
+        array_push($description, $item->timber->title);
     }
+    // implode will join the array by the ", " separator
+    $description_result = implode(", ", $description);
+
+    //Create a Stripe customer
+    $customer = $stripe->customers->create([
+        'email' => $email,
+        'source' => $token
+    ]);
+
+    //Charge the customer
+    $charge = $stripe->charges->create([
+        'amount' => $subtotal,
+        'currency' => 'eur',
+        'description' => $description_result,
+        'customer' => $customer->id
+    ]);
+
+    $total_quantity = 0;
+
+    foreach ($cart->items as $item) {
+        $total_quantity += intval($item->quantity);
+    }
+
+
+    if ($request->is_logged_in()) {
+        if (User::findByEmail($email) != null) {
+            $user = User::findByEmail($email);
+            //just add this transaction_id to the existing customer
+            if (Customer::findByUserID($user->id) != null) {
+                $customer = Customer::findByUserID($user->id);
+
+                //create new transaction and apply to existing customer
+                // the transaction table will provide info on the overall transaction and link to the customer who made the purchase
+                $transaction = new Transaction();
+                $transaction->id = $charge->id;
+                $transaction->customer_id = $customer->id;
+                $transaction->status = $charge->status;
+                $transaction->date = date("Y-m-d H:i:s");
+                $transaction->total = intval($request->session()->get("subtotal"));
+                $transaction->save();
+
+                // create transaction_timber record
+                // looping through products involved in this transaction
+                foreach ($cart->items as $item) {
+                    // transaction_timber table will store a record for each product involved in our transaction
+                    $transaction_timber = new Transaction_Timber();
+                    $transaction_timber->quantity = $item->quantity;
+                    $transaction_timber->profiling = $item->profiling;
+                    $transaction_timber->sqfootage = $item->sqfootage;
+
+                    // Database needs a 0 or 1, our checkbox passes 'on' or null, so change it to suit:
+                    $fire_rated = null;
+                    if ($item->fire_rated === null) {
+                        $fire_rated = 0;
+                    } else {
+                        $fire_rated = 1;
+                    }
+
+                    $transaction_timber->fire_rated = $fire_rated;
+                    $transaction_timber->transaction_id = $charge->id;
+                    $transaction_timber->timber_id = (Timber::findByTitle($item->timber->title))->id;
+                    $transaction_timber->save();
+                }
+            } else {
+                // if no customer exists for this user->id, this must be an admin. only customers can make purchases.
+                $request->session()->set("flash_message", "Only customers may place orders. You are logged in as an administrator.");
+                $request->session()->set("flash_message_class", "alert-warning");
+                $request->redirect("/views/checkout.php");
+            }
+        }
+    } else {
+        // user must not be logged in, so ask them to log in first
+        $request->session()->set("flash_message", "Please login to check out.");
+        $request->session()->set("flash_message_class", "alert-warning");
+        $request->redirect("/views/checkout.php");
+    }
+
+
+    // Always empty the cart after a transaction is completed
+    $cart->empty();
+    unset($_SESSION['cart']);
+    //Redirect to success page, passing charge_id and description to be retrieved via $_GET
+    $request->redirect("/views/success.php?tid=" . $charge->id . "&product=" . $charge->description);
 } else {
-    // user must not be logged in, so ask them to log in first
-    $request->session()->set("flash_message", "Please login to check out.");
-    $request->session()->set("flash_message_class", "alert-warning");
-    $request->redirect("/views/checkout.php");
-}
-
-
-// Always empty the cart after a transaction is completed
-$cart->empty();
-unset($_SESSION['cart']);
-//Redirect to success page, passing charge_id and description to be retrieved via $_GET
-$request->redirect("/views/success.php?tid=" . $charge->id . "&product=" . $charge->description);
-}
-else{
     $request->session()->set("flash_data", $request->all());
     $request->session()->set("flash_errors", $request->errors());
     $request->redirect("/views/checkout.php");
